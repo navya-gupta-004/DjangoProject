806fbac64a963bee18d825255b5ef8c131cfb250
Navya <you@example.com>
2025-06-25 12:45:23 +0530

    add url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,11 +1,12 @@
 import axios from "axios";
 import { ACCESS_TOKEN } from "./constants";
 
-const apiUrl = "/choreo-apis/djangoproject/backend/v1";
+//const apiUrl = "/choreo-apis/djangoproject/backend/v1";
 // sample JavaScript code snippet
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl,
+  baseURL: import.meta.env.VITE_API_URL,
+  //? import.meta.env.VITE_API_URL : apiUrl,
 });
 
 api.interceptors.request.use(
