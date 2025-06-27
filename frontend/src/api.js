b6e3b49a6a33884bf53b98ad5ccc4b1a574a12eb
Kajal <kajal@gmail.com>
2025-06-27 19:04:44 +0530

    frontend url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -5,8 +5,9 @@ import { ACCESS_TOKEN } from "./constants";
 // sample JavaScript code snippet
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
-  //? import.meta.env.VITE_API_URL : apiUrl,
+  baseURL: import.meta.env.VITE_API_URL
+    ? import.meta.env.VITE_API_URL
+    : import.meta.env.VITE_API_URL_DEPLOY,
 });
 
 api.interceptors.request.use(
